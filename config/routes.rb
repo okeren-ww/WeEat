19022cf59460bbc75781b7663abb651fb83d72b0
Or Keren <or.keren@wework.com>
2018-01-08 11:52:54 +0200

    Added basic functionality for model & controller for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'restaurants#index', as: 'restaurants_index'
+  resources :restaurants
+
 end
