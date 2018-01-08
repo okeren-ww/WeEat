0728bef060d6d426b7979393b3c17e23bcc037d6
Or Keren <or.keren@wework.com>
2018-01-08 11:54:28 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,4 @@ Rails.application.routes.draw do
   resources :restaurants
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180108082403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "cuisine"
+    t.integer "rating"
+    t.boolean "accepts_ten_bis"
+    t.string "address"
+    t.integer "max_delivery_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
