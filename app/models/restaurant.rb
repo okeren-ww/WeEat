5e41a95658bd92a59e9b60089ae1a77b90f9f717
Or Keren <or.keren@wework.com>
2018-01-09 16:12:16 +0200

    Modified restaurant's rating to be average of it's ratings in the db

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
   validates :name, :cuisine, :rating, :accepts_ten_bis, :address, :max_delivery_time, presence: true
   validates :rating, numericality: [greater_than_or_equal_to: 0, less_than_or_equal_to: 3]
   validates :accepts_ten_bis, inclusion: [true, false]
+  
+ def rating
+    reviews.empty? ? 0 : reviews.average(:rating)
+  end
 end
