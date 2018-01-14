905b94fe58d398e57f133e614d7fa8eb4944b8f7
Or Keren <or.keren@wework.com>
2018-01-14 15:24:55 +0200

    Fixed a migration issue & added cuisine name as index

--- db/migrate/20180114094947_change_cuisine_to_ref.rb
+++ db/migrate/20180114094947_change_cuisine_to_ref.rb
@@ -1,6 +1,6 @@
 class ChangeCuisineToRef < ActiveRecord::Migration[5.1]
   def change
-    remove_column :restaurants, :cuisine
+    remove_column :restaurants, :cuisine, :string
     add_reference :restaurants, :cuisine, index: true
 
   end
