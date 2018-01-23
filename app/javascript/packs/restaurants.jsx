325a86fb265ea0a7d3eaeb72e6c1f4269ecf7f57
Or Keren <or.keren@wework.com>
2018-01-23 15:10:44 +0200

    removed unnecessary consts

--- app/javascript/packs/restaurants.jsx
+++ app/javascript/packs/restaurants.jsx
@@ -8,7 +8,7 @@ class RestaurantCuisineRow extends React.Component {
     return (
       <tr>
         <th colSpan="2" align="left">
-            <u>{cuisine}</u>
+          <u>{cuisine}</u>
         </th>
       </tr>
     );
@@ -104,15 +104,6 @@ FilterableRestaurantTable.propTypes = {
   restaurants: PropTypes.array,
 };
 
-
-const PRODUCTS = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
-];
 
 const RESTAURANTS = [
   { name: 'Joseph & Sons', cuisine: 'British', rating: 4, accepts_ten_bis: true, max_delivery_time: 20 },
