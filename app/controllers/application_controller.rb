e7288448ccfc1c4bf06fc078c28491f42edbf547
Or Keren <or.keren@wework.com>
2018-01-24 09:12:22 +0200

    Restaurant list is retrieved from the backend and shown as simple elements using react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery prepend: true
+
+  def index
+    render inline: '', layout: 'application'
+  end
 end
