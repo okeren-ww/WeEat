db5861168cc19775c14de8664e049baaeea5a165
Or Keren <or.keren@wework.com>
2018-01-25 10:17:06 +0200

    Small design & CSS changes

--- app/javascript/packs/components/Map.jsx
+++ app/javascript/packs/components/Map.jsx
@@ -5,9 +5,9 @@ class Map extends React.Component {
         return (
             <iframe
                 width="850"
-                // height="450"
+                height="850"
                 frameBorder="0"
-                src="https://www.google.com/maps/embed/v1/place?&q=New%20York&zoom=13&key=AIzaSyCkiUHqV065uCy4s1koyM6xjMXFBCnnoCk" />
+                src="https://www.google.com/maps/embed/v1/place?&q=New%20York&zoom=13&key=AIzaSyBvZdXEk7Oo1fw0Tc9fm0CSQNtfIGTny6I" />
 
         );
     }
